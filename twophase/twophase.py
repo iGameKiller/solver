fc14bd30399db26d7109fbf1ff756b0839ff7ad0
Pedro <0035330@academico.ifmg.edu.br>
2021-12-21 18:38:54 -0300

    primeira fase semi-finalizada

--- twophase/twophase.py
+++ twophase/twophase.py
@@ -2,6 +2,7 @@ import numpy as np
 
 np.set_printoptions(suppress=True, precision=2)
 
+
 def reader(line):  # PATROCINADO
 
     # índices (posições) de expressões em strings
@@ -56,7 +57,6 @@ def reader(line):  # PATROCINADO
 
 
 def readed(objet, f_obj, restr_a, restr_op, restr_b):
-
     if objet == "MA":
         print("Objetivo: Maximizar")
     elif objet == 'MI':
@@ -70,54 +70,7 @@ def readed(objet, f_obj, restr_a, restr_op, restr_b):
     print(restr_b)
 
 
-def gauss(matrix, mayorColPos, lin, col):
-    pivots = []
-    phasetwo = False
-    for i in range(1, len(matrix)):  # montando array de linhas pivô
-        newnum = matrix[i][0] / matrix[i][mayorColPos]
-        if newnum < 0:
-            phasetwo = True
-            return phasetwo, matrix
-        pivots.append(newnum)
-
-
-    pivotMinorValue = pivots[0]
-    pivotMinorValuePos = 0
-
-    for i in range(len(pivots)):  # encontrando o menor valor do vetor de pivôs e sua posição
-        if pivots[i] < pivotMinorValue:
-            pivotMinorValue = pivots[i]
-            pivotMinorValuePos = i
-
-    pivotline = matrix[pivotMinorValuePos + 1]  # fazendo uma cópia da linha pivô da matriz
-    pivotElement = matrix[pivotMinorValuePos + 1][mayorColPos]
-
-    for i in range(lin):  # setando os novos valores para a linha pivô
-        if pivotElement > 0:
-            pivotline[i] = matrix[pivotMinorValuePos + 1][i] / pivotElement
-
-    matrix[pivotMinorValuePos + 1] = pivotline  # atualizando os valores na matriz original
-
-    for i in range(lin):  # atualizando todas as outras linhas da matriz, exceto a linha pivô
-        if i == pivotMinorValuePos + 1:
-            i + 1
-        else:
-            if i == len(matrix):
-                break
-            magicNumber = matrix[i][mayorColPos]
-            lineCopy = matrix[i]
-
-            for j in range(col):
-                if j == len(matrix[i]):
-                    break
-                else:
-                    lineCopy[j] = matrix[i][j] - (magicNumber * pivotline[j])
-
-    return phasetwo, matrix
-
-
 def constructor(f_obj, restr_a, restr_op, restr_b):
-
     A_var = []
     S_var = []
 
@@ -154,7 +107,7 @@ def constructor(f_obj, restr_a, restr_op, restr_b):
     for i in range(len(restr_b)):  # Posicionando matriz B de restrições
         matrix[i + 1][0] = restr_b[i]
 
-    if len(S_var) > 0: # Posicionando variáveis de folga
+    if len(S_var) > 0:  # Posicionando variáveis de folga
         S_matrix = np.zeros((len(restr_op), len(S_var)))
 
         aux = 0
@@ -166,7 +119,7 @@ def constructor(f_obj, restr_a, restr_op, restr_b):
             for j in range(len(S_matrix[i])):
                 matrix[i + 1][len(f_obj) + 1 + j] = S_matrix[i][j]
 
-    if len(A_var) > 0: # Posicionando variáveis artificiais
+    if len(A_var) > 0:  # Posicionando variáveis artificiais
         A_matrix = np.zeros((len(restr_op), len(A_var)))
 
         aux = 0
@@ -178,52 +131,98 @@ def constructor(f_obj, restr_a, restr_op, restr_b):
             for j in range(len(A_matrix[i])):
                 matrix[i + 1][len(f_obj) + len(S_var) + 1 + j] = A_matrix[i][j]
 
-    for i in range(len(matrix[0])-len(A_var), len(matrix[0])): # Posicionando função objetivo nova
+    for i in range(len(matrix[0]) - len(A_var), len(matrix[0])):  # Posicionando função objetivo nova
         matrix[0][i] = 1
 
-    for i in range(len(A_var)): # Posicionando soma das linhas com variáveis artificiais
-        for j in range(col-len(A_var)):
-            matrix[lin-1][j] += matrix[A_var[i][0]+1][j]
+    for i in range(len(A_var)):  # Posicionando soma das linhas com variáveis artificiais
+        for j in range(col - len(A_var)):
+            matrix[lin - 1][j] += matrix[A_var[i][0] + 1][j]
 
-    return matrix,A_var,S_var
+    return matrix, A_var, S_var
 
 
-def phasetwo():
-    print("__________________________FASE 2_________________________")
+def gauss(matrix, mayorColPos, lin, col):
+    pivots = []
+
+    for i in range(1, len(matrix)):  # montando array de linhas pivô
+        newnum = matrix[i][0] / matrix[i][mayorColPos]
+        if newnum <= 0:
+            pivots.append(0) # o Bug mora aqui
+            return matrix
+        pivots.append(newnum)
+
+    pivotMinorValue = pivots[0]
+    pivotMinorValuePos = 0
+
+    for i in range(len(pivots)):  # encontrando o menor valor do vetor de pivôs e sua posição
+        if pivots[i] < pivotMinorValue:
+            pivotMinorValue = pivots[i]
+            pivotMinorValuePos = i
+
+    pivotline = matrix[pivotMinorValuePos + 1]  # fazendo uma cópia da linha pivô da matriz
+    pivotElement = matrix[pivotMinorValuePos + 1][mayorColPos]
+
+    for i in range(lin):  # setando os novos valores para a linha pivô
+        if pivotElement > 0:
+            pivotline[i] = matrix[pivotMinorValuePos + 1][i] / pivotElement
+
+    matrix[pivotMinorValuePos + 1] = pivotline  # atualizando os valores na matriz original
+
+    for i in range(lin):  # atualizando todas as outras linhas da matriz, exceto a linha pivô
+        if i == pivotMinorValuePos + 1:
+            i + 1
+        else:
+            if i == len(matrix):
+                break
+            magicNumber = matrix[i][mayorColPos]
+            lineCopy = matrix[i]
+
+            for j in range(col):
+                if j == len(matrix[i]):
+                    break
+                else:
+                    lineCopy[j] = matrix[i][j] - (magicNumber * pivotline[j])
+
+    return matrix
+
 
 def simplextwophase(objet, f_obj, restr_a, restr_op, restr_b):
 
     matrix, A_var, S_var = constructor(f_obj, restr_a, restr_op, restr_b)
-
+    print("____________TABLEAU INICIAL____________")
+    print(matrix)
     lin = len(restr_op) + 2
     col = len(f_obj) + len(S_var) + len(A_var) + 1
-    print("__________________________TABLEAU INICIAL_________________________")
-    print(matrix)
-    allminorequalzero = False
-    phase = False
+    allessequalzero = False
     itcounter = 0
-    print("__________________________FASE 1_________________________")
 
-    while allminorequalzero == False:
+    while allessequalzero == False:
 
         itcounter = itcounter + 1
-        mayorvalue = matrix[0][0]  # Procura o maior número na ultima linha
+        pstvcounter = 0
         mayorColPos = 0
+        majorvalue = 0
 
-        for i in range(1, col - 1):  # encontrando o menor valor na ultima linha
+        for i in range(1, col):
+            if matrix[lin-1][i] >= 1:
+                pstvcounter = pstvcounter + 1
 
-            if matrix[lin-1][i] > mayorvalue:
-                mayorvalue = matrix[lin-1][i]
-                mayorColPos = i
+        if pstvcounter > 0:
 
-        print("____________________________ITERAÇÃO", itcounter, "_____________________________")
-        phase, matrix = gauss(matrix, mayorColPos, lin, col)
+            for i in range(1, col):
+                if matrix[lin - 1][i] > 0:
+                    if matrix[lin - 1][i] > majorvalue:
+                        majorvalue = matrix[lin - 1][i]
+                        mayorColPos = i
 
-        if phase == True:
-            phasetwo()
-            return 1
-        else:
-            print(matrix)
+            matrix = gauss(matrix, mayorColPos, lin, col)
+            print("_______________________Iteração",itcounter,'_______________________')
+            print(matrix, '\n')
+
+        elif pstvcounter == 0:
+            allessequalzero = True
+
+    exit()
 
     return matrix
 
@@ -254,11 +253,11 @@ def solver(objet, f_obj, restr_A, restr_op, restr_b, verbose=False):
             flag = 1
 
     if flag == 1:
-
         readed(objet, f_obj, restr_A, restr_op, restr_b)
+
         print("__________MÉTODO SIMPLEX DUAS FASES SELECIONADO__________")
         answer = simplextwophase(objet, f_obj, restr_A, restr_op, restr_b)
-        print(answer)
+        #print(answer)
 
 
 if __name__ == "__main__":
